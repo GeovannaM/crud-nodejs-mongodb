9af6db07cf8387a46fb94dfc6627f67afbe98379
Geovanna da Silva Mendes <geovannadasilvamendes@hotmail.com>
2020-01-29 15:55:33 -0300

    server

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(function(err, req, res, next) {
 /**
  * Get port from environment and store in Express.
  */
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '4000';
 app.set('port', port);
 
 /**
